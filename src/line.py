c2a0e85b18be4b70fd79a943a845178d12889706
Chathura Nilanka <chathuranilanka1024@gmail.com>
2020-04-23 15:35:52 +0530

    Test commit

--- src/line.py
+++ src/line.py
@@ -6,38 +6,65 @@ from geometry_msgs.msg import Twist
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from tf.transformations import quaternion_from_euler
 
+PI = 3.1415926535897
+
 
 def callback(msg):
+	angel = 90*2*PI/360
+	speed = 10*2*PI/360
+
+	move.linear.x=0
+	move.linear.y=0
+	move.linear.z=0
+	move.angular.x = 0
+	move.angular.y = 0
+
+	print "Distance between robot and wall :", msg.ranges[270]
+	
+	if msg.ranges[270] > 1.0 :
+		move.angular.z = -abs(speed)
+		t0 = rospy.Time.now().to_sec()
+    		current_angle = 0
+
+	    	while(current_angle < angle):
+        		vel_pub.publish(move)
+        		t1 = rospy.Time.now().to_sec()
+        		current_angle = speed*(t1-t0)
+		
+		move.angular.z = 0
+		vel_pub.publish(move)
+
+	if msg.ranges[270] < 1.0 :
+		move.angular.z = abs(speed)
+		t0 = rospy.Time.now().to_sec()
+    		current_angle = 0
+
+	    	while(current_angle < angle):
+        		vel_pub.publish(move)
+        		t1 = rospy.Time.now().to_sec()
+        		current_angle = speed*(t1-t0)
+		
+		move.angular.z = 0
+		vel_pub.publish(move)
+
 	while not rospy.is_shutdown():
 		print "Distance between robot and wall :" 
 		print msg.ranges[270]
 	
 		move.linear.x = 0.1
+		vel_pub.publish(move)
 		if msg.ranges[0] < 0.2:
 			move.linear.x = 0
 			print "Finished..!"
+			vel_pub.publish(move)
 
-		pub.publish(move)
 
 rospy.init_node('assignment')
 
-pub = rospy.Publisher('/initialpose', PoseWithCovarianceStamped, queue_size = 10)
 rospy.sleep(3)
-start_pose = PoseWithCovarianceStamped()
- 
-start_pose.pose.pose.position.x = 0.0
-start_pose.pose.pose.position.y = 1
-start_pose.pose.pose.position.z = 0.0
- 
-[x,y,z,w]=quaternion_from_euler(0.0,0.0,0.0)
-start_pose.pose.pose.orientation.x = x
-start_pose.pose.pose.orientation.y = y
-start_pose.pose.pose.orientation.z = z
-start_pose.pose.pose.orientation.w = w
 
-pub.publish(start_pose)
-sub = rospy.Subscriber('/scan', LaserScan, callback)
-pub = rospy.Publisher('/cmd_vel', Twist)
+scan_sub = rospy.Subscriber('/scan', LaserScan, callback)
+vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 move = Twist()
 
 rospy.spin()
